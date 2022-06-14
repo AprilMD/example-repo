f4d118d001c62d0daa385c26c00da1451014b5d2
April <aprilmdawson@hotmail.com>
2022-06-14 15:23:25 +0100

    add tests for each player and first round

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -1,17 +1,15 @@
 require_relative '../tennis_scores'
 
-def score_board(winner)
+def score_board(*winner)
     scores = {
         player1: 0, player2: 0
         }
-    if winner == 'player1'
+    if winner[0]== 'player1'
         scores[:player1] += 15
         return scores
     else
         return scores
    end
-
-
 end
 
 describe "tennis scores" do
@@ -29,9 +27,7 @@ describe "tennis scores" do
 
     it 'returns 15, 0 for player 1 wins first round' do
         expect(score_board('player1')).to eql({:player1 => 15, :player2 => 0})
-        # expect{score_board('player1')}.to output({:player1 => 15, :player2 => 0}).to_stdout
     end
-
 end
 
 
